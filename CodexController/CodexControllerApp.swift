09996c2e4b2fe6f03406f0fb1d7498859ebaad57
Soichi Furukawa <soufuru1@gmail.com>
2026-08-09 06:23:12 +0900

    Initial Commit

---
+++ CodexController/CodexControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexControllerApp.swift
+//  CodexController
+//
+//  Created by Soichi Furukawa on 2026/08/09.
+//
+
+import SwiftUI
+
+@main
+struct CodexControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodexController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodexController
+//
+//  Created by Soichi Furukawa on 2026/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
